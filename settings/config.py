47c6344a85b4ad611d38aeffdec5ef5e99cde775
tanner666 <tjg5@njit.edu>
2024-05-06 14:42:16 -0400

    updated smtp settings for production

--- settings/config.py
+++ settings/config.py
@@ -2,6 +2,7 @@ from builtins import bool, int, str
 from pathlib import Path
 from pydantic import  Field, AnyUrl, DirectoryPath
 from pydantic_settings import BaseSettings
+import os
 
 class Settings(BaseSettings):
     max_login_attempts: int = Field(default=3, description="Background color of QR codes")
@@ -38,8 +39,8 @@ class Settings(BaseSettings):
     # Email settings for Mailtrap
     smtp_server: str = Field(default='smtp.mailtrap.io', description="SMTP server for sending emails")
     smtp_port: int = Field(default=2525, description="SMTP port for sending emails")
-    smtp_username: str = Field(default='your-mailtrap-username', description="Username for SMTP server")
-    smtp_password: str = Field(default='your-mailtrap-password', description="Password for SMTP server")
+    smtp_username: str = Field(default=os.environ.get('smtp_username'), description="Username for SMTP server")
+    smtp_password: str = Field(default=os.environ.get('smtp_password'), description="Password for SMTP server")
 
 
     class Config:
